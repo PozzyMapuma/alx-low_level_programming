0989ffa3116e2e6e29dbe60593dce642c7688d90
PozzyMapuma <phozisandubs@gmail.com>
2023-04-20 14:08:04 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - A C program that prints exactly using printf function
+ *
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
