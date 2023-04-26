2a8cb7c02e6e18b065fe134c2b75c3917130ba69
PozzyMapuma <phozisandubs@gmail.com>
2023-04-26 12:42:49 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,15 +19,15 @@ void jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 				{
 					if (a >= 2 && b >= 4)
-						break;		
+						break;	
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
-					_putchar(c +48);
-					_putchar(d +48);
+					_putchar(c + 48);
+					_putchar(d + 48);
 					_putchar('\n');
 				}
 			}
-		}	
+		}
 	}
 }
