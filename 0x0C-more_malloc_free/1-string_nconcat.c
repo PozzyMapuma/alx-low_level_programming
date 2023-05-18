f2bd828b6b6799ea9a1799aff865f0845823f50d
PozzyMapuma <phozisandubs@gmail.com>
2023-05-18 12:47:13 -0700

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,31 +2,53 @@
 #include <stdlib.h>
 
 /**
- * _calloc - ...
- * @nmemb: number of members
- * @size: size
+ * string_nconcat - ...
+ * @s1: ...
+ * @s2: ...
+ * @n: ...
  *
  * Return: ...
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0, l = 0;
-	char *p;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	l = nmemb * size;
-	p = malloc(l);
-
-	if (p == NULL)
-		return (NULL);
-
-	while (i < l)
-	{
-		p[i] = 0;
+	while (s1[1])
 		i++;
-	}
 
-	return (p);
+	while (s2[1])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	while (j < l)
+	{
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
